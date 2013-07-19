66dc190f5e0f062aafcd1623218221cac970143c
CoolGirl <evgeniya-katunina@yandex.ru>
2013-07-19 13:46:25 +0400

    test class commit

---
+++ GameMechanics/src/TestClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: 1
+ * Date: 19.07.13
+ * Time: 13:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClass {
+}
