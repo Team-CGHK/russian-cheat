a9e85135a4494f1d15b92d5c54cc1480f24001a6
h0tk3y <hotkeytlt@gmail.com>
2013-07-21 21:41:14 +0400

    Card.java refinement    getCardSuit and getCardValue now take the needed suit/value from enum values array by index

--- GameMechanics/src/Card.java
+++ GameMechanics/src/Card.java
@@ -9,10 +9,10 @@ public class Card {
     }
 
     public static enum CardSuit {
-        Clubs(), Diamonds(), Hearts(), Spades();
+        Clubs, Diamonds, Hearts, Spades;
     }
     public static enum CardValue {
-        Two(), Three(), Four(), Five(), Six(), Seven(), Eight(), Nine(), Ten(), Jack(), Queen(), King(), Ace();
+        Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace;
         //TODO decide, whether we need Jokers in a deck
     }
 
@@ -25,17 +25,11 @@ public class Card {
     // cardIndex here and below is card position in 52-cards deck, for example, 11 == K♣
     public static CardSuit getCardSuit(int cardIndex) {
         int suitCode = cardIndex / (MAX_DECK_SIZE / 4);
-        for (CardSuit suit : CardSuit.values())
-            if (suit.ordinal() == suitCode)
-                return suit;
-        return null;
+        return CardSuit.values()[suitCode];
     }
 
     public static CardValue getCardValue(int cardIndex) {
         int valueCode = cardIndex % (MAX_DECK_SIZE / 4);
-        for (CardValue value : CardValue.values())
-            if (value.ordinal() == valueCode)
-                return value;
-        return null;
+        return CardValue.values()[valueCode];
     }
 }
